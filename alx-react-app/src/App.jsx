1ad0a12e6714d3692af041dadfedd4476f926ae5
Annick Ujeneza <annickujeneza4@gmail.com>
2025-10-31 18:43:01 +0100

    Added UserProfile component and used props in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,13 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile
+        name="Annick"
+        age="22"
+        bio="Loves hiking and photography"
+      />
     </div>
   );
 }
